33fee1a3f63aa62c1826bb52a0f645d415597b11
Binss04 <xuanthanh130304@gmail.com>
2023-12-02 17:04:20 +0700

    sua

--- src/Repository/GiamGiaRepository.java
+++ src/Repository/GiamGiaRepository.java
@@ -3,9 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Repository;
+import Model.HoaDon;
 import Repository.DBConnect;
 
 import Model.Phieugiamgia;
+import java.math.BigDecimal;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -20,76 +22,111 @@ import java.util.UUID;
  * @author Admin
  */
 public class GiamGiaRepository {
-    public List<Phieugiamgia> getAll() {
-        String query = "select phieu_giam_gia.Id,phieu_giam_gia.san_pham_id,phieu_giam_gia.chuong_trinh,phieu_giam_gia.muc_giam,phieu_giam_gia.so_luong,phieu_giam_gia.ngay_tao,phieu_giam_gia.ngay_bat_dau,phieu_giam_gia.ngay_ket_thuc from phieu_giam_gia";
+    private ArrayList<Phieugiamgia> list;
+    private Connection conn;
+    private Statement stm;
+    private PreparedStatement psm;
 
-        try ( Connection con = DBConnect.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-            ResultSet rs = ps.executeQuery();
-            List<Phieugiamgia> lists = new ArrayList<>();
-            while (rs.next()) {
-                Phieugiamgia gg= new Phieugiamgia(rs.getInt(1),rs.getString(2),
-                                rs.getString(3),rs.getInt(4) ,rs.getInt(5),
-                                rs.getDate(6), rs.getDate(7),rs.getDate(8));
-                lists.add(gg);
-            }
-            return lists;
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
-        }
-        return null;
+    public GiamGiaRepository() {
+        list = new ArrayList<>();
+        conn = DBConnect.getConnection();
+        GetData();    }
+
+    public ArrayList<Phieugiamgia> getList() {
+        return list;
     }
-    public void deletephieu(int Id) {
-        String sql = "DELETE FROM phieu_giam_gia WHERE Id=?";
 
-        try (Connection con = DBConnect.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
-            // Set parameter
-            ps.setObject(1, Id);
+    public void setList(ArrayList<Phieugiamgia> list) {
+        this.list = list;
+    }
 
-            // Execute the delete
-            ps.executeUpdate();
+    public void GetData() {
+        list.clear();
+        try {
+            stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery("select *  from phieu_giam_gia");
+            while (rs.next()) {
+                int ID = rs.getInt(1);
+                String maphieu = rs.getString(2);
+                int mucgiamgia = rs.getInt(3);
+                int soluong = rs.getInt(7);
+                String chuongtrinh = rs.getString(4);
+    
+                Date ngaybatdau = rs.getDate(5);
+                Date ngaykethuc = rs.getDate(6);
+                BigDecimal dk = rs.getBigDecimal(8);
+               
+                
+               Phieugiamgia pg = new Phieugiamgia(ID, maphieu, mucgiamgia, dk, soluong, chuongtrinh, ngaybatdau, ngaykethuc);
+                list.add(pg);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-        } catch (SQLException e) {
-            // Handle specific database exceptions
+    public void Add(Phieugiamgia M) {
+        list.clear();
+        try {
+            psm = conn.prepareStatement("Insert into phieu_giam_gia(ma_phieu,muc_giam_gia,chuong_trinh,ngay_bat_dau,ngay_ket_thuc,so_luong,dieukien) values (?,?,?,?,?,?,?)");
+            psm.setString(1,M.getMaPhieu());
+            psm.setInt(2, M.getMucgiamgia());
+            psm.setString(3, M.getChuongtrinh());
+            psm.setObject(4, M.getNgaybatdau());
+            psm.setObject(5, M.getNgayketthuc());
+            psm.setInt(6, M.getSoluong());
+            psm.setBigDecimal(7, M.getDk());
+            psm.execute();
+            GetData();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    
+}
+//    
+//
+    public boolean Delete(int ID) {
+        try {
+            psm = conn.prepareStatement("Delete from phieu_giam_gia where ID = ?");
+            psm.setInt(1, ID);
+            psm.execute();
+            GetData();
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public void UpdateQuantity(int ID, int soluong){
+        try {
+            psm = conn.prepareStatement("UPDATE phieu_giam_gia\n"
+                    + "SET so_luong = ?\n"
+                    + "WHERE ID = ?;");
+            psm.setInt(1, soluong);
+            psm.setInt(2, ID);
+            
+            psm.execute();
+            GetData();
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    public void GetPercentDiscount(String MaPhieu, int KhachHangID){
+        try {
+            psm = conn.prepareStatement(" SELECT muc_giam_gia,so_luong,dieukien,status FROM phieu_giam_gia pgg \n"
+                    + "right join phieu_giam_gia_chi_tiet ct on pgg.Id = ct.ID_phieu_giam_gia\n"
+                    + "where ID_Khach_hang = ? and ma_phieu = ? and (ngay_ket_thuc > CONVERT( date, GETDATE()) or ngay_ket_thuc = CONVERT(date, GETDATE()))");
+            psm.setString(1, MaPhieu);
+            psm.setInt(2, KhachHangID);
 
-    public boolean add(Phieugiamgia gg) {
-        int check = 0;
-        String query = "INSERT INTO phieu_giam_gia (san_pham_id, chuong_trinh,muc_giam,so_luong,ngay_tao, ngay_bat_dau, ngay_ket_thuc) VALUES(?,?,?,?,?,?,?)";
-                   
-        try ( Connection con = DBConnect.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-           
-            
-            ps.setObject(1, gg.getSanphamid());
-            ps.setObject(2,gg.getTenchuongtrinh());
-            ps.setObject(3, gg.getMucgiamgia());
-            ps.setObject(4, gg.getSoluong());
-            ps.setObject(5, gg.getNgaytao());
-            ps.setObject(6, gg.getNgaybatdau());
-            ps.setObject(7, gg.getNgayketthuc());
-            
-            
-            check = ps.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
-        }
-        return check > 0;
-    }
-    
+            psm.execute();
+            GetData();
 
-    public boolean delete(String Id) {
-        int check = 0;
-        String query = """
-                        DELETE FROM phieu_giam_gia              
-                        WHERE Id = ?
-                     """;
-        try ( Connection con = DBConnect.getConnection();  PreparedStatement ps = con.prepareStatement(query)) {
-            ps.setObject(1, Id);
-            check = ps.executeUpdate();
         } catch (Exception e) {
-            e.printStackTrace(System.out);
+            e.printStackTrace();
         }
-        return check > 0;
     }
 }
