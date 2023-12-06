886f468b18a1aac92abf8b7e1207b96bc7ab1435
Khiemdc <khiemdcph33789@fpt.edu.vn>
2023-12-06 15:24:33 +0700

    add model login

---
+++ src/Model/login.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Administrator
+ */
+public class login {
+
+    private String taikhoan;
+    private String matkhau;
+
+    public login() {
+    }
+
+    public login(String taikhoan, String matkhau) {
+        this.taikhoan = taikhoan;
+        this.matkhau = matkhau;
+
+    }
+
+    public String getTaikhoan() {
+        return taikhoan;
+    }
+
+    public void setTaikhoan(String taikhoan) {
+        this.taikhoan = taikhoan;
+    }
+
+    public String getMatkhau() {
+        return matkhau;
+    }
+
+    public void setMatkhau(String matkhau) {
+        this.matkhau = matkhau;
+    }
+
+}
